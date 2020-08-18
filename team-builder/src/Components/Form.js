346d170f079bc6bcb1a9add00a728c46c2ce85c1
vaidehee patel <pvaidehee@gmail.com>
2020-08-19 01:26:10 +0530

    done it's working

---
+++ team-builder/src/Components/Form.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function TeamForm(props) {
+    const { values, update, submit } = props
+
+    const onChange = evt => {
+        const { name, value } = evt.target
+        update(name, value)
+    }
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+        submit()
+    }
+
+    return (
+        <form className='form container' onSubmit={onSubmit}>
+            <div className='form-group submit'>
+                <h2>Add a Team Member's Info: </h2>
+                <button disabled={!values.name || !values.email || !values.role }>Submit</button>
+            </div>
+            <div className='form-group inputs'>
+                <h4>General Requirenments:</h4>
+                <PageDiv>
+                <label htmlFor='nameInput'>Name:&nbsp;
+                    <input
+                        id='nameInput'
+                        name='name'
+                        type='text'
+                        placeholder='Enter Name'
+                        maxLength='20'
+                        value={values.name}
+                        onChange={onChange}
+                    />
+                </label>
+                <label htmlFor='emailInput'>Email:&nbsp;
+                    <input
+                        id='emailInput'
+                        name='email'
+                        type='email'
+                        placeholder='Enter email'
+                        maxLength='20'
+                        value={values.email}
+                        onChange={onChange}
+                    />
+                </label>
+                <label>Role:&nbsp;
+                    <select name='role' value={values.role} onChange={onChange}>   
+                        <option value=''>Select a role</option>
+                        <option value='Front-End Engineer'>Front-End Engineer</option>
+                        <option value='Back-End Engineer'>Back-End Engineer</option>
+                        <option value='UX Designer'>UX Designer</option>
+                        <option value='Data Scientist'>Data Scientist</option>
+                    </select>
+                </label>
+            </PageDiv>
+            </div>
+        </form>
+    )
+} 
+
+const PageDiv = styled.div`
+display:flex;
+flex-direction:row;
+justify-content: space-around;
+padding-top: 30px;
+`;
